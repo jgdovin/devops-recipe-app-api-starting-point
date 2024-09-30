ec88c24db50b34d84c69b24fdfcbf3b42f069945
Josh Gdovin <joshgdovin@gmail.com>
2024-09-29 21:12:38 -0600

    setup github actions

--- infra/setup/outputs.tf
+++ infra/setup/outputs.tf
@@ -8,3 +8,13 @@ output "cd_user_access_key_secret" {
   description = "AWS key secret for CD user"
   sensitive   = true
 }
+
+output "ecr_repo_app" {
+  value       = aws_ecr_repository.app.repository_url
+  description = "ECR repository URL for app"
+}
+
+output "ecr_repo_proxy" {
+  value       = aws_ecr_repository.proxy.repository_url
+  description = "ECR repository URL for proxy"
+}
