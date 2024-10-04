3225268a5b51670ce811c78db9f84aff7c89b1a8
Josh Gdovin <joshgdovin@gmail.com>
2024-10-04 11:04:29 -0600

    added security group for ecs

--- infra/deploy/database.tf
+++ infra/deploy/database.tf
@@ -20,6 +20,8 @@ resource "aws_security_group" "rds" {
     from_port = 5432
     to_port   = 5432
     protocol  = "tcp"
+
+    security_groups = [ aws_security_group.ecs_service.id ]
   }
 
   tags = {
