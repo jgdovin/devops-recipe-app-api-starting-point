2c9ecf1fc523b954a139fde24373c012531964ab
Josh Gdovin <joshgdovin@gmail.com>
2024-10-04 09:02:33 -0600

    added ssm policy

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -19,6 +19,23 @@ resource "aws_iam_role_policy_attachment" "task_execution_role" {
   policy_arn = aws_iam_policy.task_execution_role_policy.arn
 }
 
+resource "aws_iam_role" "app_task_role" {
+  name               = "${local.prefix}-app-task"
+  assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
+}
+
+resource "aws_iam_policy" "task_ssm_policy" {
+  name        = "${local.prefix}-task-ssm-policy"
+  path        = "/"
+  description = "Allow ECS to interact with SSM."
+  policy      = file("./templates/ecs/task-ssm-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "task_ssm_policy" {
+  role       = aws_iam_role.app_task_role.name
+  policy_arn = aws_iam_policy.task_ssm_policy.arn
+}
+
 resource "aws_ecs_cluster" "main" {
   name = "${local.prefix}-cluster"
 }
