c8f37f7299b14decc3f262cc8d2491b34d27e155
Josh Gdovin <joshgdovin@gmail.com>
2024-10-05 10:22:52 -0600

    added target group

---
+++ infra/deploy/load_balancer.tf
@@ -0,0 +1,49 @@
+#################
+# Load Balancer #
+#################
+
+resource "aws_security_group" "lb" {
+  name        = "${local.prefix}-lb"
+  description = "Allow HTTP and HTTPS traffic to the load balancer."
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "tcp"
+    from_port = 8000
+    to_port   = 8000
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "app" {
+  name = "${local.prefix}-lb"
+  load_balancer_type = "application"
+  subnets = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+  security_groups = [aws_security_group.lb.id]
+}
+
+resource "aws_lb_target_group" "api" {
+  name     = "${local.prefix}-api"
+  port     = 8000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  target_type = "ip"
+
+  health_check {
+    path = "/api/health-check/"
+  }
+}
